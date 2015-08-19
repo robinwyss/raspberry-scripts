ca90fa507bc4b17f0dc7bc3317dd8b90e30d3614
Robin Wyss <rowyss@gmail.com>
2015-08-19 22:42:20 +0200

    fixed image transform

--- resize.py
+++ resize.py
@@ -12,7 +12,8 @@ def resizeImg(path):
     imgName = basename(path)
     img = Image.open(path)
     img = img.resize(size, Image.ANTIALIAS)
-    img = img.transpose(Image.FLIP_TOP_BOTTOM)
+    #img = img.transpose(Image.FLIP_TOP_BOTTOM)
+    img = img.transpose(Image.ROTATE_180)
     if not exists(folderSmall):
         makedirs(folderSmall)
     filePath = join(folderSmall, imgName)
